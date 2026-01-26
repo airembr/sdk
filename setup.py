d88c2cce80c39b7e755a782644ead43111d55e20
risto <risto.kowaczewski@gmail.com>
2026-01-26 18:57:51 +0100

    Bump `airembr-pararun-os` dependency to version 0.0.7 in `setup.py` and `requirements.txt`.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'durable-dot-dict>=0.0.22',
         'python-dateutil',
         'requests',
-        'airembr-pararun-os>=0.0.6',
+        'airembr-pararun-os>=0.0.7',
         'orjson',
         'pytz',
         'pytimeparse'
