2a04847930692c913f58e72aed114185db8c49dc
risto <risto.kowaczewski@gmail.com>
2026-02-17 15:38:54 +0100

    Add custom logging levels and utility functions for enhanced log handling
    Introduced `DEV_INFO`, `Q_INFO`, and `STAT` logging levels with corresponding utility functions (`dev_info`, `q_info`, and `stat`) in the custom `StackInfoLogger`. Refactored environment-based log level configuration with `_get_logging_level` for dynamic logging setup.

--- airembr/sdk/logging/log_handler.py
+++ airembr/sdk/logging/log_handler.py
@@ -6,6 +6,7 @@ import traceback
 import sys
 
 from airembr.sdk.logging.formater import CustomFormatter
+from airembr.sdk.logging.tools import Q_INFO_LEVEL, DEV_INFO_LEVEL, Q_STAT_LEVEL, _get_logging_level
 
 _env = os.environ
 
@@ -35,9 +36,9 @@ def stack_trace():
 
 class StackInfoLogger(logging.Logger):
     def error(self, msg, *args, **kwargs):
-        kwargs['stack_info'] = False
+        kwargs['stack_info'] = True
         if 'exc_info' not in kwargs:
-            kwargs['exc_info'] = False
+            kwargs['exc_info'] = True
         if msg is None:
             msg = "None"
         super().error(msg, *args, **kwargs)
@@ -47,16 +48,38 @@ class StackInfoLogger(logging.Logger):
         kwargs['exc_info'] = True
         super().error(msg, *args, **kwargs)
 
+    def q_info(self, msg, *args, **kwargs):
+        """
+        Logs a message with custom MESSAGE_LEVEL
+        """
+        if self.isEnabledFor(Q_INFO_LEVEL):
+            self._log(Q_INFO_LEVEL, msg, args, **kwargs)
+
+    def dev_info(self, msg, *args, **kwargs):
+        """
+        Logs a message with custom MESSAGE_LEVEL
+        """
+        if self.isEnabledFor(DEV_INFO_LEVEL):
+            self._log(DEV_INFO_LEVEL, msg, args, **kwargs)
+
+    def stat(self, msg, *args, **kwargs):
+        if self.isEnabledFor(Q_STAT_LEVEL):
+            self._log(Q_STAT_LEVEL, msg, args, **kwargs)
+
 
 logging.setLoggerClass(StackInfoLogger)
 logging.basicConfig(level=logging.INFO)
+logging.addLevelName(Q_INFO_LEVEL, "Q_INFO")
+logging.addLevelName(DEV_INFO_LEVEL, "DEV_INFO")
+logging.addLevelName(Q_STAT_LEVEL, "STAT")
+_logging_level = _get_logging_level(_env['LOGGING_LEVEL']) if 'LOGGING_LEVEL' in _env else logging.WARNING
 
 
 def get_logger(name, level=None):
     # Replace the default logger class with your custom class
     logger = logging.getLogger(name)
     logger.propagate = False
-    logger.setLevel(level or logging.WARNING)
+    logger.setLevel(level or _logging_level)
 
     # Console log handler
 
