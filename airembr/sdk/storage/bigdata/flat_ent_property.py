1085b812ab8a2678de0ddf0a04d6a7b8b77dbeef
risto <risto.kowaczewski@gmail.com>
2026-02-27 12:10:10 +0100

    Add `PROPERTY_ID` and `VECTOR` fields to `flat_ent_property.py`

--- airembr/sdk/storage/bigdata/flat_ent_property.py
+++ airembr/sdk/storage/bigdata/flat_ent_property.py
@@ -6,10 +6,13 @@ class FlatEntityProperty(DotDict):
     ID = 'entity.id'
     TYPE = 'entity.type'
 
+    PROPERTY_ID = "property.id"
     NAME = "property.name"
     VALUE = "property.value"
     TEXT = "property.text"
     NUMBER = "property.number"
+    VECTOR = "property.vector"
     TS = 'ts'
 
     OBSERVER_PK = 'observer.pk'
+
