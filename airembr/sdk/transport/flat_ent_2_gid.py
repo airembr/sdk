05482e1e8c0bc8e59d8a1c56d114ec948b5eb1d2
risto <risto.kowaczewski@gmail.com>
2026-03-17 07:58:51 +0100

    Add sys_ent_2_gid table support and mapping schema
    Introduce the sys_ent_2_gid table to the schema with its mapping schema and indexing configuration. Added a FlatEntity2Gid wrapper class for easier access to sys_ent_2_gid attributes and properties. These changes enable structured handling of entity-to-GID relationships with persistent indexing.

---
+++ airembr/sdk/transport/flat_ent_2_gid.py
@@ -0,0 +1,8 @@
+from durable_dot_dict.dotdict import DotDict
+
+
+class FlatEntity2Gid(DotDict):
+    ENTITY_PK = 'entity.pk'
+    ENTITY_TYPE = 'entity.type'
+    ENTITY_GID = 'entity.gid'
+    ENTITY_GID_TYPE = 'entity.gid_type'
