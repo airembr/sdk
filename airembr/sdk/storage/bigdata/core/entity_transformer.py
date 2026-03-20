69d0bd2a10cb1e56859a56b65e73bc07b69352f5
risto <risto.kowaczewski@gmail.com>
2026-03-20 10:38:05 +0100

    Refactor entity and relation processing in property adapters
    Removed unused functions, improved formatting, and enhanced logic for handling relations and entities in property extraction and transformation. Added support for processing relation attributes such as labels and types, along with improved DISTINCT counts for property matching.

--- airembr/sdk/storage/bigdata/core/entity_transformer.py
+++ airembr/sdk/storage/bigdata/core/entity_transformer.py
@@ -6,6 +6,7 @@ from airembr.sdk.common.date import now_in_utc
 from airembr.sdk.service.hashes.hash import md5
 from airembr.sdk.storage.bigdata.flat_ent_property import FlatEntityProperty
 from airembr.sdk.transport.flat_relation import FlatRelation
+from airembr.sdk.transport.flat_observation_entity import ObservationEntity
 
 
 def _compute_properties_from_traits(relation: DotDict):
@@ -31,20 +32,107 @@ def _compute_properties_from_traits(relation: DotDict):
 def compute_entity_property_from_entities(storage_context_entities: List[DotDict],
                                           with_property_id: bool = False, exclude_relations: bool = False):
     for entity in storage_context_entities:
+
         if isinstance(entity, FlatRelation):
             # For for_rel
             if exclude_relations:
                 continue
+
             entity_properties = _compute_properties_from_traits(entity)
         else:
             #  for_entity(FlatEntity)
             entity_properties = _compute_properties_from_traits(entity)
+
+        # Yield addition data
+
+        if isinstance(entity, FlatRelation):
+
+            # Yield label for realtion
+            label = entity.get(FlatRelation.REL_LABEL, None)
+            if label:
+                row = {
+                    FlatEntityProperty.PK: entity['entity.pk'],
+                    FlatEntityProperty.ID: entity['entity.id'],
+                    FlatEntityProperty.TYPE: entity['entity.type'],
+
+                    FlatEntityProperty.NAME: "_label",
+                    FlatEntityProperty.VALUE: label,
+                    FlatEntityProperty.TEXT: label,
+                    FlatEntityProperty.NUMBER: None,
+                    FlatEntityProperty.VECTOR: None,
+                    FlatEntityProperty.TS: now_in_utc(),
+
+                    FlatEntityProperty.OBSERVER_PK: entity[ObservationEntity.OBSERVER_PK]
+                }
+
+                row[FlatEntityProperty.PROPERTY_ID] = md5(
+                    f"{row[FlatEntityProperty.OBSERVER_PK]}"
+                    f"-{row[FlatEntityProperty.PK]}"
+                    f"-{row[FlatEntityProperty.TYPE]}"
+                    f"-{row[FlatEntityProperty.VALUE]}"
+                )
+                yield row
+
+            # Yield type for relation
+            event_type = entity.get(FlatRelation.REL_TYPE, None)
+            if event_type:
+                row = {
+                    FlatEntityProperty.PK: entity['entity.pk'],
+                    FlatEntityProperty.ID: entity['entity.id'],
+                    FlatEntityProperty.TYPE: entity['entity.type'],
+
+                    FlatEntityProperty.NAME: "_type",
+                    FlatEntityProperty.VALUE: event_type,
+                    FlatEntityProperty.TEXT: event_type,
+                    FlatEntityProperty.NUMBER: None,
+                    FlatEntityProperty.VECTOR: None,
+                    FlatEntityProperty.TS: now_in_utc(),
+
+                    FlatEntityProperty.OBSERVER_PK: entity[ObservationEntity.OBSERVER_PK]
+                }
+
+                row[FlatEntityProperty.PROPERTY_ID] = md5(
+                    f"{row[FlatEntityProperty.OBSERVER_PK]}"
+                    f"-{row[FlatEntityProperty.PK]}"
+                    f"-{row[FlatEntityProperty.TYPE]}"
+                    f"-{row[FlatEntityProperty.VALUE]}"
+                )
+                yield row
+
+        elif isinstance(entity, ObservationEntity):
+
+            # Yield label for entity
+            label = entity.get(ObservationEntity.ENTITY_LABEL, None)
+            if label:
+                row = {
+                    FlatEntityProperty.PK: entity['entity.pk'],
+                    FlatEntityProperty.ID: entity['entity.id'],
+                    FlatEntityProperty.TYPE: entity['entity.type'],
+
+                    FlatEntityProperty.NAME: "_label",
+                    FlatEntityProperty.VALUE: label,
+                    FlatEntityProperty.TEXT: label,
+                    FlatEntityProperty.NUMBER: None,
+                    FlatEntityProperty.VECTOR: None,
+                    FlatEntityProperty.TS: now_in_utc(),
+
+                    FlatEntityProperty.OBSERVER_PK: entity[ObservationEntity.OBSERVER_PK]
+                }
+
+                row[FlatEntityProperty.PROPERTY_ID] = md5(
+                    f"{row[FlatEntityProperty.OBSERVER_PK]}"
+                    f"-{row[FlatEntityProperty.PK]}"
+                    f"-{row[FlatEntityProperty.TYPE]}"
+                    f"-{row[FlatEntityProperty.VALUE]}"
+                )
+                yield row
 
         _observer_pk = entity[FlatRelation.OBSERVER_PK]
         for row in entity_properties:
             # TODO this can be an issue as TS is added late in the pipeline
             row[FlatEntityProperty.TS] = now_in_utc()
             row[FlatEntityProperty.OBSERVER_PK] = _observer_pk
+
             if with_property_id:
                 # This hash will keep historic values as well as it hashes value
                 row[FlatEntityProperty.PROPERTY_ID] = md5(
--- airembr/sdk/storage/bigdata/core/observation_converter.py
+++ airembr/sdk/storage/bigdata/core/observation_converter.py
@@ -1,5 +1,6 @@
 from airembr.sdk.storage.bigdata.flat_ent_history import FlatEntityHistory
 from airembr.sdk.transport.flat_obs_2_entity import FlatObs2Entity
+from airembr.sdk.transport.flat_relation import FlatRelation
 
 
 def get_obs_2_entity_object(observation_id, entity, session_id: str = None):
@@ -9,5 +10,16 @@ def get_obs_2_entity_object(observation_id, entity, session_id: str = None):
         FlatObs2Entity.ENTITY_PK: entity[FlatEntityHistory.ENTITY_PK],
         FlatObs2Entity.ENTITY_TYPE: entity[FlatEntityHistory.ENTITY_TYPE],
         FlatObs2Entity.ENTITY_DATA_HASH: entity[FlatEntityHistory.DATA_HASH],
+        FlatObs2Entity.SESSION_ID: session_id
+    }
+
+
+def get_rel_2_entity_object(observation_id, relation: FlatRelation, session_id: str = None):
+    return {
+        FlatObs2Entity.OBSERVATION_ID: observation_id,
+        FlatObs2Entity.ENTITY_ID: relation[FlatRelation.ENTITY_ID],
+        FlatObs2Entity.ENTITY_PK: relation[FlatRelation.ENTITY_PK],
+        FlatObs2Entity.ENTITY_TYPE: relation[FlatRelation.ENTITY_TYPE],
+        FlatObs2Entity.ENTITY_DATA_HASH: relation[FlatRelation.DATA_HASH],
         FlatObs2Entity.SESSION_ID: session_id
     }
