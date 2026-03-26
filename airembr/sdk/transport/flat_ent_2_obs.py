642ad7f85994be69a708c634ad26aaac4d8a4512
risto <risto.kowaczewski@gmail.com>
2026-03-26 12:17:46 +0100

    Add `ENTITY_TYPE` key to `FlatEntity2Observation` class
    Extended the `FlatEntity2Observation` mapping by introducing the `ENTITY_TYPE` key to support entity type handling in observation processing.

--- airembr/sdk/transport/flat_ent_2_obs.py
+++ airembr/sdk/transport/flat_ent_2_obs.py
@@ -4,3 +4,4 @@ from durable_dot_dict.dotdict import DotDict
 class FlatEntity2Observation(DotDict):
     ENTITY_PK = 'entity.pk'
     OBSERVATION_ID = 'observation.id'
+    ENTITY_TYPE = "entity.type"
