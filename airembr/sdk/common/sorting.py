ff2412e1355eae24d8ab3df6e9ad274925eaf486
risto <risto.kowaczewski@gmail.com>
2026-04-13 12:52:06 +0200

    Fixed retrival error.

--- airembr/sdk/common/sorting.py
+++ airembr/sdk/common/sorting.py
@@ -6,3 +6,5 @@ def sort_dicts_by_id_order(data: List[Dict[str, Any]],
                            sort_key: str = "id"):
     order_map = {id_: i for i, id_ in enumerate(sort_by_ids)}
     data.sort(key=lambda x: order_map.get(x.get(sort_key), float("inf")))
+
+    return data
