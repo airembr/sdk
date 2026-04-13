14584dbb2fd1db8d347b884b45a63edc871cfcb3
risto <risto.kowaczewski@gmail.com>
2026-04-13 11:01:08 +0200

    Added observation collection.

--- airembr/sdk/chat_client.py
+++ airembr/sdk/chat_client.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from collections import defaultdict
 from datetime import datetime
 from typing import Optional, Tuple, List, Union, Dict, Set
@@ -15,9 +16,11 @@ from airembr.sdk.service.remote.airembr_api import AirembrApi
 from airembr.sdk.service.time.time import now_in_utc
 
 
-def entity(type: str, traits: Optional[dict] = None, label: Optional[str] = None,
-           id: Optional[str] = None) -> 'AiRembrChatEntity':
-    return AiRembrChatEntity(
+def entity(type: str,
+           traits: Optional[dict] = None,
+           label: Optional[str] = None,
+           id: Optional[str] = None) -> 'AiRembrEntity':
+    return AiRembrEntity(
         entity=ObservationEntity(
             instance=Instance.type(type, id),
             label=label,
@@ -25,12 +28,14 @@ def entity(type: str, traits: Optional[dict] = None, label: Optional[str] = None
         )
     )
 
-def event(type: str, traits: Optional[dict] = None, label: Optional[str] = None):
+
+def event(type: str, label: str, traits: Optional[dict] = None) -> 'AiRembrEvent':
     return AiRembrEvent(
         type=type,
         traits=traits,
         label=label
     )
+
 
 class _Chats:
     def __init__(self):
@@ -49,6 +54,7 @@ class _Chats:
     def __len__(self):
         return sum([len(item) for item in self._chats.values()])
 
+
 class AiRembrEvent:
 
     def __init__(self, type: str, traits: Optional[dict] = None, label: Optional[str] = None):
@@ -56,7 +62,17 @@ class AiRembrEvent:
         self.traits = traits
         self.type = type
 
-class AiRembrChatEntity:
+    def to_entity(self, type='event') -> 'AiRembrEntity':
+        return AiRembrEntity(
+            entity=ObservationEntity(
+                instance=Instance(type),
+                label=self.label,
+                traits=self.traits
+            )
+        )
+
+
+class AiRembrEntity:
 
     def __init__(self, entity: ObservationEntity):
         self.link: InstanceLink = InstanceLink.create()
@@ -68,10 +84,10 @@ class AiRembrChatEntity:
 
 class AiRembrChatPeer:
 
-    def __init__(self, client: 'AiRembrChatClient', observation: 'AirembrObservation', entity: AiRembrChatEntity):
+    def __init__(self, client: 'AiRembrChatClient', observation: 'AirembrObservation', entity: AiRembrEntity):
         self.observation = observation
         self.client = client
-        self.entity: AiRembrChatEntity = entity
+        self.entity: AiRembrEntity = entity
         self.chats: _Chats = _Chats()
 
     def message(self, message: str,
@@ -112,9 +128,9 @@ class AirembrChat:
         self.chat_ttl = chat_ttl
         self.chat_id = chat_id
         self.peers: Set[AiRembrChatPeer] = set()
-        self.entities: Set[AiRembrChatEntity] = set()
+        self.entities: Set[AiRembrEntity] = set()
 
-    def peer(self, entity: AiRembrChatEntity) -> AiRembrChatPeer:
+    def peer(self, entity: AiRembrEntity) -> AiRembrChatPeer:
         peer = AiRembrChatPeer(
             self.client,
             self.observation,
@@ -124,7 +140,7 @@ class AirembrChat:
         self.peers.add(peer)
         return peer
 
-    def entity(self, entity: Union[AiRembrChatEntity, Set[AiRembrChatEntity]]):
+    def entity(self, entity: Union[AiRembrEntity, Set[AiRembrEntity]]):
         if isinstance(entity, set):
             self.entities.update(entity)
         else:
@@ -177,17 +193,91 @@ class AirembrChat:
 
 class AirembrObservation:
 
-    def __init__(self, client, observer: AiRembrChatEntity, id: Optional[str] = None,
-                 session_id: Optional[str] = None):
+    def __init__(self,
+                 client: 'AiRembrChatClient',
+                 observer: AiRembrEntity,
+                 label: str,
+                 description: Optional[str] = None,
+                 id: Optional[str] = None,
+                 session_id: Optional[str] = None,
+                 traits: Optional[dict] = None):
+
         self.client = client
         self.observation_id = id or str(uuid4())
         self.session_id = session_id or str(uuid4())
         self.observer_link = observer.link
+        self.label = label
+        self.traits = traits
+        self.description = description
+        self.entities: Optional[Set[AiRembrEntity]] = None
+        self.relations: List[ObservationRelation] = []
 
     def chat(self,
              chat_id: str,
              chat_ttl: int = 60 * 60) -> AirembrChat:
         return AirembrChat(self.client, self, chat_id, chat_ttl)
+
+    def fact(self,
+             actor: AiRembrEntity,
+             relation: AiRembrEvent,
+             objects: Optional[List[AiRembrEntity]] = None,
+             description: Optional[str] = None,
+             summary: Optional[str] = None,
+             ts:Optional[datetime] = None,
+             tags: Optional[Set[str]] = None,
+             ):
+        if not self.entities:
+            raise ValueError(f"Please set observations entities before adding facts.")
+
+        rel = ObservationRelation(
+            type=relation.type,
+            label=relation.label,
+            traits=relation.traits,
+            actor=actor.link,
+            actor_label=actor.entity.label,
+            objects=[object.link for object in objects] if objects is not None else [],
+            semantic=Semantic(
+                description=description,
+                summary=summary,
+            ),
+            ts = ts,
+            tags=list(tags) if tags else [],
+        )
+
+        self.relations.append(rel)
+
+    def context(self, entities: Set[AiRembrEntity]) -> 'AirembrObservation':
+        self.entities = entities
+        return self
+
+    def _get_observation(self):
+        if self.entities:
+            entities = {ent.link: ent.entity for ent in self.entities}
+        else:
+            entities = {}
+
+        return Observation(
+            label=self.label,
+            text=self.description,
+            traits=self.traits,
+            observer=self.observer_link,
+            source=Entity(id=self.client.source_id),
+            entities=entities,
+            relation=self.relations
+        )
+
+    def remember(self,
+                 realtime: Optional[str] = None,
+                 skip: Optional[str] = None,
+                 context: Optional[str] = None) -> Tuple[QueryStatus, MemorySessions]:
+
+        observation = self._get_observation()
+        transport = AirembrApi(self.client.api)
+
+        return transport.remember(
+            observation.model_dump(mode="json"),
+            realtime, skip, False, context
+        )
 
 
 class AiRembrChatClient:
@@ -207,10 +297,22 @@ class AiRembrChatClient:
         self.chat_id: Optional[str] = None
 
     def observation(self,
-                    observer: AiRembrChatEntity,
+                    observer: AiRembrEntity,
+                    label: str,
                     id: Optional[str] = None,
-                    session_id: Optional[str] = None) -> AirembrObservation:
-        return AirembrObservation(self, observer, id, session_id)
+                    session_id: Optional[str] = None,
+                    description: Optional[str] = None,
+                    traits=None) -> AirembrObservation:
+        if traits is None:
+            traits = {}
+
+        return AirembrObservation(self,
+                                  observer,
+                                  label,
+                                  description,
+                                  id,
+                                  session_id,
+                                  traits)
 
     @staticmethod
     def get_references() -> Tuple[InstanceLink, InstanceLink, InstanceLink]:
