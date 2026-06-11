ade2af3b334432c58c6981de684f5cdbaf5d367a
risto <risto.kowaczewski@gmail.com>
2026-06-11 19:23:35 +0200

    Enable functionality checks based on license validation
    Integrated license validation to conditionally load specific routes, workflows, and plugin installations. This ensures restricted features are only accessible when a valid license is present, improving compliance and modularity.

--- airembr/system/adapter/metadata/mysql/interface/dao/workflow.py
+++ airembr/system/adapter/metadata/mysql/interface/dao/workflow.py
@@ -1,5 +1,8 @@
-from dagor.interface.workflow.entrypoint import load_workflow
+from airembr.system.license.license_verifier import system_license
+
+if system_license.valid:
+    from dagor.interface.workflow.entrypoint import load_workflow
 
 
-async def get_workflow(workflow_id: str):
-    return await load_workflow(workflow_id)
+    async def get_workflow(workflow_id: str):
+        return await load_workflow(workflow_id)
--- airembr/system/process/installation/installer.py
+++ airembr/system/process/installation/installer.py
@@ -1,6 +1,5 @@
-from dagor.interface.plugin.entrypoint import install_default_plugins
-
 from airembr.model.system.installer.credentials import Credentials
+from airembr.system.license.license_verifier import system_license
 from airembr.system.process.logging.log_handler import get_installation_logger
 from airembr.system.adapter.bigdata.big_data_adapter import *
 from airembr.system.process.installation.md_install_manager import MetaDataInstallManager
@@ -9,14 +8,15 @@ logger = get_installation_logger(__name__)
 
 
 async def install_system(credentials: Credentials):
-
     if sys_config.installation_token and sys_config.installation_token != credentials.token:
         raise PermissionError("Installation forbidden. Invalid installation token.")
 
     admin = await MetaDataInstallManager().install_metadata_database(credentials, version=sys_config.version)
 
-    logger.info(f"Installing plugins on startup")
-    await install_default_plugins()
+    if system_license.valid:
+        from dagor.interface.plugin.entrypoint import install_default_plugins
+        logger.info(f"Installing plugins on startup")
+        await install_default_plugins()
 
     staging_install_result, production_install_result = await bd_install_adapter.install_big_data_database(credentials)
     staging_install_result['admin'] = admin
