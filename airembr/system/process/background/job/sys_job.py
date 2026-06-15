75f65953a1ab78046373d943ce4888f87d4e2b9a
risto <risto.kowaczewski@gmail.com>
2026-06-15 09:21:29 +0200

    Refactor and relocate background worker and monitoring modules
    Refactored the background worker system, moving monitoring, logging, and metric modules to a more centralized and consistent structure under `airembr/system/process/background`. Removed obsolete files and updated references in Dockerfiles and scripts to reflect the new structure. This improves code organization and maintainability.

---
+++ airembr/system/process/background/job/sys_job.py
@@ -0,0 +1,66 @@
+import asyncio
+import os
+from asyncio import sleep
+from time import time
+
+from srd.config import StarRocksConfig
+
+from airembr.model.system.context import ServerContext
+from airembr.system.process.ai.chunker.text_chunk_embedder import chunk_texts
+from airembr.system.process.stitching.stitch import stitch
+from airembr.system.process.logging.log_handler import get_logger
+from airembr.system.adapter.bigdata.tenant.tenant_adapter import load_tenant_database_and_context
+from airembr.sdk.ai.config import embedding_host
+
+logger = get_logger(__file__)
+
+
+async def main():
+    t = time()
+
+    config = StarRocksConfig()
+
+    logger.dev_info(
+        f"Config: STARROCKS_HOST = {config.starrocks_database_uri} (env: {os.environ.get('STARROCKS_HOST', None)})")
+
+    logger.dev_info(
+        f"Config: EMBEDDING_HOST = {embedding_host} (env: {os.environ.get('EMBEDDING_HOST', None)})")
+
+    # Scan database names and produce context
+    async for database, context in load_tenant_database_and_context():
+        logger.info(f"Context: {context}, Database: {database}")
+        with ServerContext(context):
+            await asyncio.gather(
+                stitch(context, database),
+                chunk_texts(context),
+            )
+
+            # await chunk_texts(context)
+
+            # # logger.info("Text summarization...")
+            # # await summarize(context)
+            # #
+            # # # Embed texts
+            # # logger.info("Text embeddings...")
+            # await embed(context)
+            #
+            # # Stitch entity state
+            # await stitch(context, database)
+            #
+            # # Embed texts
+            # # logger.info("Text entities...")
+            # await extract_entities(context)
+            # #
+            # # # Stitch entity state
+            # # await stitch(context, database)
+            # #
+            # # # Embedd again
+            # # # logger.info("Text embeddings...")
+            # # await embed(context)
+
+    await sleep(2)
+
+    logger.info(f"Finished in {time() - t}")
+
+
+asyncio.run(main())
