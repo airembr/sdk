1d9f098e84e4ba6f05ef6a45af4de1c127ebee7b
risto <risto.kowaczewski@gmail.com>
2026-06-17 13:55:16 +0200

    Remove IMAP and ICAL bridge configurations from default bridges
    Deleted IMAP and ICAL bridge definitions including their forms, manuals, and constants. Removed `imap_bridge` and `ical_bridge` from `os_default_bridges` list, leaving only core bridges (JS, REST, webhook, redirect).

--- airembr/system/preconfig/setup_bridges.py
+++ airembr/system/preconfig/setup_bridges.py
@@ -5,7 +5,6 @@ from airembr.model.gui.form import Form, FormGroup, FormField, FormComponent
 
 _local_dir = os.path.dirname(__file__)
 
-IMAP_BRIDGE_ID = 'imap-91be-476d-a4e5-a495159f'
 REDIRECT_BRIDGE_ID = 'redirect-91be-476d-a4e5-1b2d7e005403'
 API_BRIDGE_ID = "api-4ff3-4e08-9a86-72c0195fa95d"
 JS_BRIDGE_ID = "js-adff-6e08-9a86-72c0195fa95d"
@@ -131,108 +130,9 @@ redirect_bridge = Bridge(
     ])
 )
 
-with open(os.path.join(_local_dir, "bridges/manual/imap_manual.md"), "r", encoding="utf-8") as fh:
-    imap_manual = fh.read()
-
-imap_bridge = Bridge(
-    id=IMAP_BRIDGE_ID,
-    type='imap',
-    name="E-mail IMAP Bridge",
-    description="Reads emails and registers events.",
-    manual=imap_manual,
-    form=Form(groups=[
-        FormGroup(name="E-mail owner",
-                  fields=[
-                      FormField(
-                          id="owner_name",
-                          name="Please type E-mail Owner Name and Surname",
-                          component=FormComponent(type="text", props={"label": "Owner Name and Surname"}),
-                          required=True
-                      ),
-                  ]),
-        FormGroup(
-            name="E-mail IMAP  Configuration",
-            description="The Redirect Bridge requires information about the destination URL and the event to be triggered when the link is clicked.",
-            fields=[
-                FormField(
-                    id="imap_server",
-                    name="Please type imap server domain.",
-                    component=FormComponent(type="text", props={"label": "E-Map Server"})
-                ),
-                FormField(
-                    id="email_account",
-                    name="Email account.",
-                    component=FormComponent(type="text", props={"label": "Email Account"})
-                ),
-                FormField(
-                    id="email_password",
-                    name="Email password",
-                    description="Please type e-mail password.",
-                    component=FormComponent(type="password", props={"label": "E-mail password"})
-                )
-            ]),
-
-        FormGroup(
-            name="Initial Fetch in Days",
-            description="How many e-mails should be fetched on source start. This allows to index previous e-mails. Define in days.",
-            fields=[
-                FormField(
-                    id="initial",
-                    name="Initial Fetch",
-                    component=FormComponent(type="text", props={"label": "How many emails should I get on start."})
-                )
-            ])
-    ])
-)
-
-with open(os.path.join(_local_dir, "bridges/manual/ical_manual.md"), "r", encoding="utf-8") as fh:
-    ical_manual = fh.read()
-
-ical_bridge = Bridge(
-    id=IMAP_BRIDGE_ID,
-    type='ical',
-    name="Calendar ICAL Bridge",
-    description="Reads meetings and registers events from ICAL file.",
-    manual=imap_manual,
-    form=Form(groups=[
-        FormGroup(
-            name="ICAL Configuration",
-            description="The Redirect Bridge requires information about the URL ICAL file.",
-            fields=[
-                FormField(
-                    id="account_email",
-                    name="Please Type Calendar Account Email.",
-                    component=FormComponent(type="text", props={"label": "E-mail"}),
-                    required=True
-                ),
-                FormField(
-                    id="account_owner",
-                    name="Please Type Calendar Owner Name and Surname",
-                    component=FormComponent(type="text", props={"label": "Owner Name and Surname"}),
-                    required=True
-                ),
-                FormField(
-                    id="calendar_name",
-                    name="Please Type Calendar Name, e.g. Personal, Business, etc.",
-                    component=FormComponent(type="text", props={"label": "Calendar Name"}),
-                    required=True
-                ),
-                FormField(
-                    id="ical_file",
-                    name="Please type ical URL.",
-                    component=FormComponent(type="text", props={"label": "ICAL URL"}),
-                    required=True
-                ),
-            ]),
-
-    ])
-)
-
 os_default_bridges = [
     open_js_source_bridge,
     open_rest_source_bridge,
     open_webhook_source_bridge,
-    redirect_bridge,
-    imap_bridge,
-    ical_bridge
+    redirect_bridge
 ]
